d8acb7daeefe0eee8d211ffac95ced37f7f4ba5a
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-06-03 05:21:10 -0700

    routing

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <div>
+      dashborad
+    </div>
+  )
+}
+
+export default pages
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import Header from './Header'
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>Hello</body>
+      <Header/>
+      {children}
+      <Footer/>
     </html>
   )
 }
